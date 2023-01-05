00d002ba92fcb96182da4cc36ffe72b5e76b2eba
leo <lp916@ic.ac.uk>
2023-01-05 19:33:08 +0100

    chain class

--- application.py
+++ application.py
@@ -1,8 +1,23 @@
 import web3
+import ast
+import os
+from dotenv import load_dotenv
+
+
+class Chain:
+    def __init__(self, network):
+        load_dotenv()
+        self.network = network
+        self.socket = ast.literal_eval(os.getenv("ALCHEMY_SOCKETS"))[network]
+        self.w3 = web3.Web3(web3.Web3.HTTPProvider(self.socket))
+
+    def isConnected(self):
+        print(f"connected: ", self.w3.isConnected())
 
 
 def main():
-    w3 = web3.Web3(web3.Web3.HTTPProvider(avado_url))
+    ch = Chain("polygon")
+    ch.isConnected()
 
 
 if __name__ == "__main__":
